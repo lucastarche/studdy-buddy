c5dfd4d2a56f5b0eee7716be96906ba13422392d
jeremiasudesa <jfigueiredopaschmann@udesa.edu.ar>
2023-04-01 17:50:12 -0300

    balblablabla

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,53 +1,84 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:studdy_buddy/firebase_options.dart';
-
+import 'package:image_network/image_network.dart';
 import '../routes.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    final heading = theme.textTheme.headlineLarge;
-    final loginText = theme.textTheme.headlineSmall;
+    final headingStyle = theme.textTheme.headlineMedium;
+    final loginTextStyle = theme.textTheme.titleLarge;
 
     return AuthStateListener<OAuthController>(
-      listener: ((oldState, state, controller) {
+      listener: (oldState, state, controller) {
         if (state is SignedIn || state is UserCreated) {
-          Navigator.pushReplacementNamed(context, Routes.home.name);
+          final user = FirebaseAuth.instance.currentUser;
+          if (user != null) {
+            Navigator.pushReplacementNamed(context, Routes.home.name);
+            _showUserInfoDialog(context, user);
+          }
+          //Navigator.pushReplacementNamed(context, Routes.home.name);
         }
-
         return null;
-      }),
+      },
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               "Bienvenido a Studdy Buddy!",
-              style: heading,
+              style: headingStyle,
             ),
             const SizedBox(height: 32),
             Text(
               "Inicia sesión para continuar",
-              style: loginText,
+              style: loginTextStyle,
             ),
             const SizedBox(height: 40),
-            SizedBox(
-              width: 320,
-              child: OAuthProviderButton(
-                provider: GoogleProvider(clientId: googleClientID),
-              ),
+            OAuthProviderButton(
+              key: const Key('oauth-button'),
+              provider: GoogleProvider(clientId: googleClientID),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showUserInfoDialog(BuildContext context, User user) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('User Information'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(child: Image(image: NetworkImage(user.photoURL ?? ''))),
+                const SizedBox(height: 16),
+                Text('Name: ${user.displayName ?? "N/A"}'),
+                Text('Email: ${user.email ?? "N/A"}'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/jeremias/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/jeremias/Documents/studdy-buddy" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/jeremias/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/jeremias/Documents/studdy-buddy"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/jeremias/Documents/studdy-buddy/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/jeremias/Documents/studdy-buddy/lib/main.dart"
+)
